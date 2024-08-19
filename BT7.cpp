0a85a0f102551204c7abbf04f9776f073d8c1497
unset <unset>
2024-08-19 16:31:34 +0700

    Refactor SieveOfEratosthenes function to calculate the product of prime numbers

--- BT7.cpp
+++ BT7.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
 #include <vector>
+#include <iomanip>
 
-std::vector<int> SieveOfEratosthenes(int n, long long &sum)
+std::vector<int> SieveOfEratosthenes(int n, long long &sum, double &product)
 {
+    sum = 0;
+    product = 1;
     std::vector<int> prime;
     int *sieve = new int[n + 1]{0};
 
@@ -15,6 +18,7 @@ std::vector<int> SieveOfEratosthenes(int n, long long &sum)
                 sieve[j] = i;
             }
             sum += i;
+            product *= i;
             prime.push_back(i);
         }
     }
@@ -26,15 +30,16 @@ int main()
 {
     int n;
     long long sum = 0;
-    long long product = 1;
+    double product = 1;
     std::cout << "Nhap n: ";
     std::cin >> n;
     std::cout << "Cac so nguyen to <= " << n << " la: ";
-    std::vector<int> sieve = SieveOfEratosthenes(n, sum);
+    std::vector<int> sieve = SieveOfEratosthenes(n, sum, product);
     for (int i = 0; i < sieve.size(); i++)
     {
         std::cout << sieve[i] << " ";
     }
     std::cout << "\nTong cac so nguyen to <= " << n << " la: " << sum;
+    std::cout << "\nTich cac so nguyen to <= " << n << " la: " << std::fixed << std::setprecision(0) << product;
     return 0;
 }
