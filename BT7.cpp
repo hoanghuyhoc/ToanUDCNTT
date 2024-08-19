cfca79f0fc61fcf6f33fb989cca7ba7c35c4bfcb
unset <unset>
2024-08-19 19:14:52 +0700

    Refactor SieveOfEratosthenes function to calculate the product of prime numbers

--- BT7.cpp
+++ BT7.cpp
@@ -2,14 +2,24 @@
 #include <vector>
 #include <iomanip>
 
-std::vector<int> SieveOfEratosthenes(int n, long long &sum, double &product)
+std::vector<int> SieveOfEratosthenes(int m, int n, long long &sum, double &product)
 {
     sum = 0;
     product = 1;
     std::vector<int> prime;
     int *sieve = new int[n + 1]{0};
 
-    for (int i = 2; i <= n; i++)
+    for (int i = 2; i < m; i++)
+    {
+        if (sieve[i] == 0)
+        {
+            for (int j = i * i; j <= n; j += i)
+            {
+                sieve[j] = i;
+            }
+        }
+    }
+    for (int i = m; i <= n; i++)
     {
         if (sieve[i] == 0)
         {
@@ -28,18 +38,22 @@ std::vector<int> SieveOfEratosthenes(int n, long long &sum, double &product)
 
 int main()
 {
-    int n;
+    int m, n;
     long long sum = 0;
     double product = 1;
+    std::cout << "Cac so nguyen to trong doan [m, n] (m>2)\n";
+    std::cout << "Nhap m: ";
+    std::cin >> m;
     std::cout << "Nhap n: ";
     std::cin >> n;
-    std::cout << "Cac so nguyen to <= " << n << " la: ";
-    std::vector<int> sieve = SieveOfEratosthenes(n, sum, product);
+
+    std::cout << "Cac so nguyen to trong doan [" << m << ", " << n << "] la: ";
+    std::vector<int> sieve = SieveOfEratosthenes(m, n, sum, product);
     for (int i = 0; i < sieve.size(); i++)
     {
         std::cout << sieve[i] << " ";
     }
-    std::cout << "\nTong cac so nguyen to <= " << n << " la: " << sum;
-    std::cout << "\nTich cac so nguyen to <= " << n << " la: " << std::fixed << std::setprecision(0) << product;
+    std::cout << "\nTong cac so nguyen to trong doan [" << m << ", " << n << "] la: " << sum;
+    std::cout << "\nTich cac so nguyen to trong doan [" << m << ", " << n << "] la: " << std::fixed << std::setprecision(0) << product;
     return 0;
 }
